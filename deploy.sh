714b233d7d18ca832161a7e624bc716df83c7dcc
Vishal Gulati <vishal.rvg@gmail.com>
2019-11-01 09:07:01 +0530

    deploy.sh modified

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,17 @@ npm run build
 # navigate into the build output directory
 cd dist
 
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:vishalgulati/vue-grid-poc.git master:gh-pages
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:VishalGulati/vue-grid-poc.git master:gh-pages
 
 cd -
